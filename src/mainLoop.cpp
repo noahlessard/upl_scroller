4f05950f026c25a66b783ad4cd39f6eed91fea96
noahlessard <noah.lessard@me.com>
2026-02-23 23:18:51 -0600

    clean up "reels"

--- src/mainLoop.cpp
+++ src/mainLoop.cpp
@@ -1,43 +1,44 @@
 #include "scroll.h"
 #include "mainLoop.h"
+#include <thread>
+#include <chrono>
 
-struct Reel {
-    ncplane* plane;
-    int      x;
-    int      len;
-};
+ncplane* std_plane;
+unsigned win_height, win_width;
+notcurses* nc;
 
-static void run_reel(notcurses* nc, Reel& r, int y, int w, float speed) {
-    while (true) {
-        ncplane_move_yx(r.plane, y, r.x);
+static void run_reel(std::string_view text, float speed) {
+    returnToNormalBoarder(false);
+    struct ncplane_options opts = {
+        .y    = (int)win_height - 1,
+        .x    = (int)win_width,
+        .rows = 1,
+        .cols = (unsigned)text.size(),
+    };
+    ncplane* rplane = ncplane_create(std_plane, &opts);
+    ncplane_set_fg_rgb8(rplane, 0, 0, 0);
+    ncplane_set_bg_rgb8(rplane, 0, 255, 0);
+    ncplane_putstr(rplane, text.data());
+    
+    int x = (int)win_width;
+    int len = (int)text.size();
+    
+    // Scroll once
+    while (x + len > 0) {
+        ncplane_move_yx(rplane, (int)win_height - 1, x);
         notcurses_render(nc);
-        if (--r.x + r.len <= 0) r.x = w;
-        usleep((useconds_t)(speed * 1000000));
+        --x;
+        std::this_thread::sleep_for(std::chrono::milliseconds((int)(speed * 1000)));
     }
+    
+    ncplane_destroy(rplane);
+    returnToNormalBoarder(true);
+
 }
 
-int main() {
-    setlocale(LC_ALL, "");
-    notcurses_options opts = {};
-    notcurses* nc           = notcurses_init(&opts, nullptr);
-    ncplane*   std_plane    = notcurses_stdplane(nc);
+static void returnToNormalBoarder(bool textAtBottom) {
+    ncplane_erase(std_plane);
 
-    unsigned win_height, win_width;
-    ncplane_dim_yx(std_plane, &win_height, &win_width);
-
-    std::string_view t1 = "UPL TRAIN CAM";
-    std::string_view t2 = "24 HRS A DAY";
-    auto [s1, s2] = calc_sync_speeds(t1.length(), t2.length(), win_width, SPEED);
-
-    int y1 = (int)(win_height - BIG_FONT_ROW_SIZE) / 2 - 4;
-    int y2 = (int)(win_height - BIG_FONT_ROW_SIZE) / 2 + 6;
-
-    std::vector<Scroll> scrolls = {
-        { t1, y1, s1, SCROLL_RIGHT, win_width, 0x800000, 0xFFFFFF },
-        { t2, y2, s2, SCROLL_LEFT,  win_width, 0x00FF00, 0x000000 },
-    };
-
-    // Draw a box around the entire screen
     uint64_t chan = 0;
     ncchannels_set_fg_rgb8(&chan, 0, 0, 0);
     ncchannels_set_bg_rgb8(&chan, 0, 255, 0);
@@ -47,26 +48,29 @@ int main() {
     ncplane_set_fg_rgb8(std_plane, 0, 0, 0);
     ncplane_set_bg_rgb8(std_plane, 0, 255, 0);
     ncplane_putstr_yx(std_plane, 0, 2, " UPL TRAIN CAM ");
-    ncplane_putstr_yx(std_plane, (int)win_height - 1, 2, " 24 HRS A DAY ");
-
-    ncplane_putstr_yx(std_plane, (int)win_height - 10, 10, " 24 HRS A DAY ");
-
-    std::string_view reel_text = "  UPL TRAIN CAM  *  24 HRS A DAY  *  LIVE BROADCAST  *  ";
-    struct ncplane_options rpopts = {
-        .y    = (int)win_height - 1,
-        .x    = (int)win_width,
-        .rows = 1,
-        .cols = (unsigned)reel_text.size(),
-    };
-    ncplane* rplane = ncplane_create(std_plane, &rpopts);
-    ncplane_set_fg_rgb8(rplane, 0, 0, 0);
-    ncplane_set_bg_rgb8(rplane, 0, 255, 0);
-    ncplane_putstr(rplane, reel_text.data());
-    Reel reel = { rplane, (int)win_width, (int)reel_text.size() };
+    if (textAtBottom)
+        ncplane_putstr_yx(std_plane, (int)win_height - 1, 2, " 24 HRS A DAY ");
 
     notcurses_render(nc);
-    //run_scrolls(nc, std_plane, scrolls);
-    run_reel(nc, reel, (int)win_height - 1, (int)win_width, SPEED);
+
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    notcurses_options opts = {};
+    nc = notcurses_init(&opts, nullptr);
+    std_plane = notcurses_stdplane(nc);
+
+    ncplane_dim_yx(std_plane, &win_height, &win_width);
+
+    returnToNormalBoarder(true);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    //run_reel("  UPL TRAIN CAM  *  24 HRS A DAY   ", 0.1);
+
+    run_reel(" mister beast better get me the new 6 7 matcha labubu clairo vinyl dubai chocoalte trump announced dead at 79 sports car", 0.15);
+    
+    
     notcurses_stop(nc);
     return 0;
 }
--- src/mainLoop.h
+++ src/mainLoop.h
@@ -1,6 +1,9 @@
 #pragma once
-
+#include <string_view>
 constexpr unsigned BIG_FONT_ROW_SIZE  = 20;
 constexpr unsigned BIG_FONT_COLS_SIZE = 22;
 constexpr int      BIG_FONT_SPACING   = 10;
 constexpr double   SPEED              = 0.1;
+
+static void run_reel(std::string_view text, float speed);
+static void returnToNormalBoarder(bool textAtBottom);
